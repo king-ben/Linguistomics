d1445184717c055e61fa6e3801be158e85daecc8
John Huelsenbeck <johnh@berkeley.edu>
2026-01-14 18:12:52 +0100

    Tiny delta.

--- TongueTwister2/Msg.cpp
+++ TongueTwister2/Msg.cpp
@@ -6,12 +6,12 @@
 
 void Msg::error(std::string s) {
 
-	std::cout << "Error: " << s << std::endl;
-	std::cout << "Exiting program" << std::endl;
+	std::cout << "   Error: " << s << std::endl;
+	std::cout << "   Exiting program" << std::endl;
 	std::exit(1);
 }
 
 void Msg::warning(std::string s) {
 
-	std::cout << "Warning: " << s << std::endl;
+	std::cout << "   Warning: " << s << std::endl;
 }
