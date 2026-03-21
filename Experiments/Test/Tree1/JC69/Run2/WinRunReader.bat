228923350fad031a10ff708a40a44826e71166fa
Shawn McCreight <shawn.mccreight@gmail.com>
2026-03-20 17:14:26 -0700

    Tonguetwister2 folder

--- Experiments/Test/Tree1/JC69/Run2/WinRunReader.bat
+++ Experiments/Test/Tree1/JC69/Run2/WinRunReader.bat
@@ -1,2 +1,2 @@
-C:/Users/shawn/Documents/Code/Linguistomics/TongueTwisterReader/x64/release/tonguetwisterreader.exe -i C:/Users/shawn/Documents/Code/Linguistomics/Experiments/Test/Tree1/JC69/Run2/Execute -b 15000 -o C:/Users/shawn/Documents/Code/Linguistomics/Experiments/Test/Tree1/JC69/Run2/Execute/out
+C:/Users/shawn/Documents/Code/Linguistomics/TongueTwisterReader2/x64/release/tonguetwisterreader.exe -i C:/Users/shawn/Documents/Code/Linguistomics/Experiments/Test/Tree1/JC69/Run2/Execute -b 15000 -o C:/Users/shawn/Documents/Code/Linguistomics/Experiments/Test/Tree1/JC69/Run2/Execute/out
 pause
