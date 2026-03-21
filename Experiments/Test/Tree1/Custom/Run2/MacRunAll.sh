d4a7bee8d1d20e87f5b520fe5f0d8d5966831533
Shawn McCreight <shawn.mccreight@gmail.com>
2026-03-20 17:51:44 -0700

    Batch files

--- Experiments/Test/Tree1/Custom/Run2/MacRunAll.sh
+++ Experiments/Test/Tree1/Custom/Run2/MacRunAll.sh
@@ -1,3 +1,3 @@
-../../../../../TongueTwister/tonguetwister -d config.json -o Execute/out
-../../../../../TongueTwisterReader/ttread -i Execute -b 15000 -o Execute/out
+../../../../../TongueTwister2/tonguetwister -d config.json -o Execute/out
+../../../../../TongueTwisterReader2/ttread -i Execute -b 15000 -o Execute/out
 read -rsp $'Press enter to continue...\n'
--- Experiments/Test/Tree1/JC69/Run2/MacRunReader.sh
+++ Experiments/Test/Tree1/JC69/Run2/MacRunReader.sh
@@ -1,2 +1,2 @@
-../../../../../TongueTwisterReader/ttread -i Execute -b 15000 -o Execute/out
+../../../../../TongueTwisterReader2/ttread -i Execute -b 15000 -o Execute/out
 read -rsp $'Press enter to continue...\n'
