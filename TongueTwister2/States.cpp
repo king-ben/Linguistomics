41a965858e37b8f84fbb921fed290081bd71d38d
John Huelsenbeck <johnh@berkeley.edu>
2026-03-22 20:50:55 +0100

    TT2 prints information on state labels.

--- TongueTwister2/States.cpp
+++ TongueTwister2/States.cpp
@@ -24,6 +24,12 @@ void States::initialzieStates(void) {
     if (jd.hasKey("NumberOfStates") == false)
         Msg::error("Could not find the number of states in JSON object");
     numStates = jd.getValue<int>("NumberOfStates");
+    
+    // read the information on the state names, if available
+    if (jd.hasKey("SegmentIPA") == true)
+        segmentIPA = jd.getValue<std::vector<std::string>>("SegmentIPA");
+    if (jd.hasKey("SegmentName") == true)
+        stateNames = jd.getValue<std::vector<std::string>>("SegmentName");
 
     // add the partitioning of the state set into natural classes
     statePartitions = nullptr;
@@ -35,6 +41,19 @@ void States::print(void) {
 
     std::cout << "   State Information" << std::endl;
     std::cout << "   * Number of states: " << numStates << std::endl;
+    if (stateNames.size() == numStates || segmentIPA.size() == numStates)
+        {
+        std::cout << "   * State labels:" << std::endl;
+        for (size_t i=0; i<numStates; i++)
+            {
+            std::cout << "     " << i << " : ";
+            if (stateNames.size() == numStates)
+                std::cout << stateNames[i] << " ";
+            if (segmentIPA.size() == numStates == numStates)
+                std::cout << segmentIPA[i] << " ";
+            std::cout << std::endl;
+            }
+        }
     if (statePartitions != nullptr)
         statePartitions->print();
     std::cout << std::endl;
--- TongueTwister2/States.hpp
+++ TongueTwister2/States.hpp
@@ -9,18 +9,20 @@ class Partition;
 class States {
 
     public:
-                        States(void);
-                       ~States(void);
-        size_t          getNumStates(void) { return numStates; }
-        Partition*      getPartition(void) { return statePartitions; }
-        void            initialzieStates(void);
-        void            print(void);
+                                    States(void);
+                                   ~States(void);
+        size_t                      getNumStates(void) { return numStates; }
+        Partition*                  getPartition(void) { return statePartitions; }
+        void                        initialzieStates(void);
+        void                        print(void);
     
     private:
-                        States(States&);
-        States&         operator=(const States&);
-        Partition*      statePartitions;
-        size_t          numStates;
+                                    States(States&);
+        States&                     operator=(const States&);
+        Partition*                  statePartitions;
+        size_t                      numStates;
+        std::vector<std::string>    stateNames;
+        std::vector<std::string>    segmentIPA;
 };
 
 #endif
