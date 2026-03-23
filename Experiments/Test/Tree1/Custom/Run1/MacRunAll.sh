5a59ce174780045cd4127f27200ed96c3399e5d9
Shawn McCreight <shawn.mccreight@gmail.com>
2026-03-23 16:40:23 -0700

    Updated shell scripts for test

--- Experiments/Test/Tree1/Custom/Run1/MacRunAll.sh
+++ Experiments/Test/Tree1/Custom/Run1/MacRunAll.sh
@@ -1,2 +1,14 @@
+cd ../../../../../TongueTwister2
+make
+cd ../TongueTwisterReader2
+make
+cd ../Experiments/Test/Tree1/Custom/Run1
+rm Execute/out.config
+rm Execute/*.json
+rm Execute/out.tre
+rm Execute/out.tsv
 ../../../../../TongueTwister2/tonguetwister -d config.json -o Execute/out
+rm Execute/consensus.tre
+rm Execute/alignments.nytril
+rm Execute/alignments_full.nytril
 ../../../../../TongueTwisterReader2/ttread -b 0.1 -o Execute/out -f true -d Execute -n Execute
